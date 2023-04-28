f1cda096584a4f5572b68a24d724f740981067c6
httpE2Barao <elias.barao@hotmail.com>
2023-04-28 19:40:58 -0300

    ajustando

--- books/src/components/header/index.tsx
+++ books/src/components/header/index.tsx
@@ -6,34 +6,62 @@ const Header = () => {
         <header className="header header-mobile">
         <div className="container">
       
-          <input type="checkbox" className="container__botao" id="menu"/>
+          <input 
+            type="checkbox"
+            className="container__botao"
+            id="menu"
+          />
           <label htmlFor="menu">
             <span className="menu-hamburger"></span>
           </label>
       
           <ul className="lista-menu">
-            <li className="lista-menu__titulo">Categorias</li>
-            <li className="lista-menu__item">
-              <a href="#" className="lista-menu__link">Programação</a>
+            <li className="lista-menu__titulo">
+              Categorias
             </li>
             <li className="lista-menu__item">
-              <a href="#" className="lista-menu__link">Front-end</a>
+              <a href="#" className="lista-menu__link">
+                Programação</a>
             </li>
             <li className="lista-menu__item">
-              <a href="#" className="lista-menu__link">Back-end</a>
+              <a href="#" className="lista-menu__link">
+                Front-end</a>
             </li>
             <li className="lista-menu__item">
-              <a href="#" className="lista-menu__link">Dados</a>
+              <a href="#" className="lista-menu__link">
+                Back-end</a>
+            </li>
+            <li className="lista-menu__item">
+              <a href="#" className="lista-menu__link">
+                Dados</a>
             </li>
           </ul>
       
-          <img className="icone-main" src="./imagens/icone-main.svg" alt="Logo Books"/>
+          <img 
+            className="icone-main"
+            src="./imagens/icone-main.svg"
+            alt="Logo Books"
+          />
       
         </div>
         <div className="container">
-            <a href="#"><img className="fav icone" src="./imagens/icone-favoritos.svg" alt="Meu Favoritos"/></a>
-            <a href="#"><img className="icone" src="./imagens/icone-sacola.svg" alt="Minha Sacola"/></a>
-            <a href="#"><img className="icone" src="./imagens/icone-perfil.svg" alt="Meu Perfil"/></a>
+            <a href="#">
+              <img 
+              className="fav icone" 
+              src="./imagens/icone-favoritos.svg" 
+              alt="Meu Favoritos"/>
+            </a>
+            <a href="#">
+              <img 
+              className="icone" 
+              src="./imagens/icone-sacola.svg" 
+              alt="Minha Sacola"/>
+            </a>
+            <a href="#">
+              <img className="icone" 
+              src="./imagens/icone-perfil.svg" 
+              alt="Meu Perfil"/>
+            </a>
         </div>
         </header>
       
@@ -46,16 +74,22 @@ const Header = () => {
         </div>
         <div className="containerDireita">
           <div>
-            <img src="./imagens/icone-sacola.svg" alt="Ícone sacola"/>
+            <img 
+              src="./imagens/icone-sacola.svg" 
+              alt="Ícone sacola"
+            />
             <p>Minha sacola</p>
           </div>
           <div>
-            <img src="./imagens/icone-perfil.svg" alt="Ícone perfil"/>
+            <img 
+              src="./imagens/icone-perfil.svg" 
+              alt="Ícone perfil"
+            />
             <p>Meu perfil</p>
           </div>
         </div>
         </header>
     </>
-    )
+  )
 }
 export default Header
